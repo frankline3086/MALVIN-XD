615851823da0cadc31e770b11017e1a961ae36b4
frankline3086 <franklineodhiambo246@gmail.com>
2025-07-26 11:27:59 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~xnsRRArA#KipiS4_9GXD2asnbsjPv1ImQPkAhezjlnSqwx88vMdU ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "254745800517", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
